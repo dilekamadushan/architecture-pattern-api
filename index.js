2e8495fb9485cd12584a7fd32d33f26a56dfe13e
dileka <dileka@grubtech.com>
2021-10-09 13:14:14 +0530

    added express initiated repository

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const serverless = require('serverless-http');
 
 const express = require('express')
-
+const cors = require('cors');
+const bodyParser = require( 'body-parser');
 const app = express()
+
+// Enable CORS requests from *.sysco.net origins
+app.use(cors());
+
+
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
+    console.log("Request here");
 
-    res.send('Hello World!')
+    res.send(JSON.stringify({name:'Hello World!'}));
 
 })
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+## To start offline
+
+sls offline start --noPrependStageInUrl
